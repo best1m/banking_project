bc5ed07b9c326805220899e946a048dccfdd4170
“userbest1m <kthiswell@gmail.com>
2018-08-08 01:50:28 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int choiceNum;
+enum
+{
+    CREATE_ACCOUNT = 1,
+    DEPOSIT,
+    WITHDRAW,
+    SHOWALL_ACCOUNT,
+    EXIT
+};
+
+struct Account{
+    int id;
+    char name[20];
+    int balance;
+};
+
+Account accArr[100];
+int accNum = 0;
+
+void showMenu();
+void createAccount();
+
+int main()
+{
+
+    while (1)
+    {
+        showMenu();
+        cin >> choiceNum;
+        cout << "selected : " << choiceNum << endl;
+        switch (choiceNum)
+        {
+        case CREATE_ACCOUNT:
+            createAccount();
+            break;
+        case DEPOSIT:
+            cout << "hello 2" << endl;
+            break;
+        case WITHDRAW:
+            cout << "hello 3" << endl;
+            break;
+        case SHOWALL_ACCOUNT:
+            cout << "hello 4" << endl;
+            break;
+        case EXIT:
+            return 0;
+        default:
+            cout << "wrong selected number!" << endl;
+        }
+    }
+
+    return 0;
+}
+
+void showMenu()
+{
+    cout << "------MENU-------" << endl;
+    cout << "1. create account" << endl;
+    cout << "2. deposit" << endl;
+    cout << "3. withdraw" << endl;
+    cout << "4. show all account" << endl;
+    cout << "5. exit" << endl;
+}
+
+void createAccount()
+{
+    int id;
+    char name[20];
+    int balance;
+
+    cout << "Insert ID : ";
+    cin >> id;
+    cout << "Insert NAME : ";
+    cin >> name;
+    cout << "Insert balance : ";
+    cin >> balance;
+
+    accArr[accNum].id = id;
+    strcpy(accArr[accNum].name, name);
+    accArr[accNum].balance = balance;
+
+    cout << "created account is : " << accArr << endl;
+
+    accNum++;
+
+
+}
